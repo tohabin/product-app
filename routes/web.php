c2deb4ccf4d7388e905975783d30d026d7260155
Toha <39374275+tohabin@users.noreply.github.com>
2024-11-11 23:10:53 +0600

    update router

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,24 @@ use App\Http\Controllers\ProductController;
 
 
 
-Route::get('/', [ProductController::class, 'index'])->name('products.index'); // List all products
-Route::get('/products', [ProductController::class, 'index'])->name('products.index'); // List all products
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // Show form to create a new product
-Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // Store a new product
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show'); // Show a specific product
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit'); // Show form to edit a product
-Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update'); // Update a product
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy'); // Delete a product
+
+// List all products
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+
+// Show the form to create a new product
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+
+// Store a new product
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+// Show a specific product by its ID
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+// Show the form to edit an existing product
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+// Update an existing product
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+// Delete a product
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
